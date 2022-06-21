4c08f3b50a00729e59a8f2db4c05ec20566bee0d
Suslenkov59 <107192083+Suslenkov59@users.noreply.github.com>
2022-06-21 21:18:48 +0500

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import s from './Header.module.css';
 
 const Header = () => {
-    return <header className={s.header}>
-        <a>Telegram beta</a>
-        <img src='https://free-png.ru/wp-content/uploads/2021/01/telegram_cvet-d23c11fa-340x334.png' />
-    </header>
+    return <div className={s.header}>
+        <a href="/profile" className={s.logo}>Telegram beta</a>
+        <img src='https://free-png.ru/wp-content/uploads/2021/01/telegram_cvet-d23c11fa-340x334.png'/>
+        <div className={s.header2}>
+            <a href="/contact" className={s.active}>Что нибудь потом</a>
+            <a href="/settings">Настройки</a>
+        </div>
+    </div>
 }
+
 
 export default Header;
